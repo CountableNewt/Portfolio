3ddeb5ca979739c7c806205f96897aa7f71a1064
Samuel Clemente <samuel@theclementes.com>
2024-02-10 16:26:04 -0500

    Webhook-handling script
    This is the initial commit to listen for GitHub Webhooks

---
+++ www/scripts/webhook-handler.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const crypto = require('crypto');
+const exec = require('child_process').exec;
+require('dotenv').config();
+
+const app = express();
+const port = process.env.WEBHOOK_PORT || 3001;
+const secret = process.env.GITHUB_SECRET;
+
+app.use(express.json());
+
+app.post('/webhook', (req, res) => {
+    const payload = JSON.stringify(req.body);
+    if(!payload) {
+        return res.status(400).send('Payload required');
+    }
+
+    const sig = req.get('X-Hub-Signature') || '';
+    const hmac = crypto.createHmac('sha1', secret);
+    const digest = Buffer.from('sha1=' + hmac.update(payload).digest('hex'), 'utf8');
+    const checksum = Buffer.from(sig, 'utf8');
+
+    if (checksum.length !== digest.length || !crypto.timingSafeEqual(digest, checksum)) {
+        return res.status(400).send('Invalid signature');
+    }
+
+    exec('git pull', (err, stdout, stderr) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send(err.message);
+        }
+        console.log(stdout);
+        console.error(stderr);
+        res.status(200).send('Updated successfully');
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
