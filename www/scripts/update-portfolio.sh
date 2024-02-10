7a1f7b7fe7604d19c9e7beb54da9918e703e3abc
Samuel Clemente <samuel@theclementes.com>
2024-02-10 18:39:20 -0500

    Update portfolio scripts and webhook handler

--- www/scripts/update-portfolio.sh
+++ www/scripts/update-portfolio.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+sudo su
 cd ~/Portfolio
-sudo git pull
+git pull
--- www/scripts/webhook-handler.js
+++ www/scripts/webhook-handler.js
@@ -1,8 +1,10 @@
+// Description: This script is used to handle the GitHub webhook and update the portfolio website when a push event is received.
 const express = require('express');
 const crypto = require('crypto');
 const exec = require('child_process').exec;
 require('dotenv').config();
 
+// Create the server
 const app = express();
 const port = process.env.WEBHOOK_PORT || 3001;
 const secret = process.env.GITHUB_SECRET;
@@ -12,22 +14,25 @@ app.use(express.json());
 
 // Routes
 app.post('/webhook', (req, res) => {
+    // Validate the payload
     const payload = JSON.stringify(req.body);
     if(!payload) {
         return res.status(400).send('Payload required');
     }
 
+    // Validate the signature
     const sig = req.get('X-Hub-Signature') || '';
     const hmac = crypto.createHmac('sha1', secret);
     const digest = Buffer.from('sha1=' + hmac.update(payload).digest('hex'), 'utf8');
     const checksum = Buffer.from(sig, 'utf8');
 
+    // Compare the two buffers in constant time
     if (checksum.length !== digest.length || !crypto.timingSafeEqual(digest, checksum)) {
         return res.status(400).send('Invalid signature');
     }
 
     // Execute the shell script
-    exec("sudo ./update-portfolio.sh", (err, stdout, stderr) => {
+    exec('git pull', (err, stdout, stderr) => {
         if (err) {
             console.error(err);
             return res.status(500).json({ details: 'Internal Server Error', error: err.message, payload: req.body });
@@ -38,6 +43,7 @@ app.post('/webhook', (req, res) => {
     });
 });
 
+// Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
